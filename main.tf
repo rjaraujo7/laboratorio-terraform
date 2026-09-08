49b36f5841a63250598fa3e494939630639f4791
Ricardo A <rjaraujo23@gmail.com>
2026-09-08 16:53:42 +0200

    feat: configure remote s3 backend with dynamodb

--- main.tf
+++ main.tf
@@ -1,9 +1,18 @@
 terraform {
+  required_version = ">= 1.5.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+
+  backend "s3" {
+    bucket         = "laboratorio-terraform-state-374320036543"
+    key            = "laboratorio/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
   }
 }
 
