69b13779d832e83c89810f544a2a00e4d8c89293
Ricardo A <rjaraujo23@gmail.com>
2026-09-09 17:01:43 +0200

    fix:protocol egress v2

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["93.156.194.72/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
