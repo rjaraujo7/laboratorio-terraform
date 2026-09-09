9a7e6119423177500481545505606dc8e96b88ad
Ricardo A <rjaraujo23@gmail.com>
2026-09-09 11:50:08 +0200

    feat: add internet gateway and public route table to vpc

--- main.tf
+++ main.tf
@@ -49,6 +49,32 @@ resource "aws_subnet" "mi_subnet" {
   }
 }
 
+resource "aws_internet_gateway" "mi_gw" {
+  vpc_id = aws_vpc.mi_vpc.id
+
+  tags = {
+    Name = "laboratorio-igw"
+  }
+}
+
+resource "aws_route_table" "mi_rt" {
+  vpc_id = aws_vpc.mi_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mi_gw.id
+  }
+
+  tags = {
+    Name = "laboratorio-rt"
+  }
+}
+
+resource "aws_route_table_association" "mi_rta" {
+  subnet_id      = aws_subnet.mi_subnet.id
+  route_table_id = aws_route_table.mi_rt.id
+}
+
 resource "aws_security_group" "mi_sg" {
   name        = "permitir-shh-http"
   description = "Permitir trafico SSH entrante y http web"
