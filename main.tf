dcac0cc9cd5fa940153e18a0e181f32b3e59661d
Ricardo A <rjaraujo23@gmail.com>
2026-09-14 12:11:43 +0200

    feat: change name vpc and subnet

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "network" {
   source      = "./modules/network"
   vpc_cidr    = "10.0.0.0/16"
   subnet_cidr = "10.0.1.0/24"
-  environment = "laboratorio"
+  environment = var.environment
 }
 
 module "compute" {
